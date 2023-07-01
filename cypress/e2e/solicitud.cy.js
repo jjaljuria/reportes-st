3808afb05a569964c44ddfd7c0ea3ae5c29c58fa
jjaljuria <josejavieral13@gmail.com>
2023-07-01 11:34:14 -0400

    created cypress test on SolicitudHome

---
+++ cypress/e2e/solicitud.cy.js
@@ -0,0 +1,23 @@
+/* globals cy */
+
+describe('formulario de solicitud de soporte técnico', () => {
+  it('should show page', () => {
+    cy.visit('localhost:3000/solicitud')
+  })
+
+  it('should have title "Solicitud de Soporte Técnico"', () => {
+    cy.visit('localhost:3000/solicitud')
+    cy.contains('Solicitud de Soporte Técnico')
+  })
+
+  it.only('should have fields "oficina, coordinacion, usuario, solicitado por, descripcion de la solicitud" and button "Enviar"', () => {
+    cy.visit('localhost:3000/solicitud')
+    cy.get('[name="oficina"]').type('otic')
+    cy.get('[name="coordinacion"]').type('soporte')
+    cy.get('[name="usuario"]').type('jose')
+    cy.get('[name="solicitado"]').type('jose')
+    cy.get('[name="descripcion"]').type('problemas')
+
+    cy.contains('Enviar').click()
+  })
+})
