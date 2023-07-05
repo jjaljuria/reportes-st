7dfe249836662e6344e45355db951797b21b6dde
jjaljuria <josejavieral13@gmail.com>
2023-07-05 13:45:50 -0400

    created migrate

--- src/controllers/SolicitudController.js
+++ src/controllers/SolicitudController.js
@@ -1,7 +1,9 @@
+import {Request} from '../database/models';
+
 export function home(req, res) {
     res.render('solicitudHome')
 }
 
-export function espera(req, res) {
+export async function espera(req, res) {
     return res.render('espera')
 }
