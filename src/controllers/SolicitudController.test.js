fcbfe559e6ffb751a7994cbf36ae14873095e8f3
jjaljuria <josejavieral13@gmail.com>
2023-07-10 13:59:08 -0400

    created test

--- src/controllers/SolicitudController.test.js
+++ src/controllers/SolicitudController.test.js
@@ -2,11 +2,7 @@ import {jest} from '@jest/globals'
 import * as SolicitudController from './SolicitudController.js'
 import request from 'supertest'
 import app from '../app.js'
-import Request from '../database/models/request.js';
-
-jest.mock('../database/models/request.js')
-console.log(Request.findAll);
-
+import Request from '../database/models/request.js'
 
 describe('SolicitudController', () => {
     it('should SolicitudController exists', () => {
@@ -24,9 +20,21 @@ describe('SolicitudController', () => {
         expect(SolicitudController.espera).toBeDefined()
     })
 
-    it('should have route GET /espera', async() => {
+    it('should have route GET /espera and return 200', async() => {
+        Request.findAll = jest.fn(()=> Promise.resolve([]))
         await request(app).get('/espera')
         .expect(200)
+    })
+
+    it('should GET /espera return 500 when throw error with database', async ()=>{
+        Request.findAll = jest.fn()
+        Request.findAll.mockRejectedValueOnce(new Error('Fail'))
+        
+        // para que no me tire el error en la consola
+        console.error = jest.fn()
+        
+        await request(app).get('/espera')
+        .expect(500)
     })
 
     it('should have createRequest and route POST /solicitud', async ()=>{
